1adb4f4e7f7c3bc4a50ae29216f7fcdfd0769210
jaguar-person <derekliman63@gmail.com>
2023-02-17 07:03:30 -0500

    upload two projects

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -10,7 +10,7 @@ const Who: FC = () => {
         data-scroll
         data-scroll-speed="2"
         className="w-[225px] h-[225px] rounded-full"
-        src="/avatar.jpg"
+        src="/derek.jpg"
         alt=""
       />
       <div>
@@ -21,10 +21,10 @@ const Who: FC = () => {
         {/* Hide my age :v */}
         <p data-scroll className="text-lg text-gray-200" id="story">
           <p>My name is Derek.</p>
-          I am 27 years old, Full Stack Web Developer who likes coding and football.
-          As far as now, developed elegant and remarkable projects using various Stacks such as MERN, MEAN, Laravel, Django, NextJS.
-          My personality is <span>Creative</span> and <span>Active</span>.
-          I like challenging myself with new skills so that I maintain up-to-date...
+          I am 28 years old, JavaScript Engineer who likes coding and football.
+          As far as now, developed elegant and remarkable projects using various JS frameworks such as React, Next, Vue, Nuxt, Svelte, Angular, React Native, Node, Express, Nest.</p>
+          <p>In addition, I am familiar with integration with other frameworks such as Laravel, Django, and Solidity.</p>
+          <p>My personality is <span>Creative</span> and <span>Active</span> and I like challenging myself with new skills so that I maintain up-to-date...
         </p>
       </div>
     </div>
